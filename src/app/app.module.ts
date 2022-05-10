49b8c768edb29704f188d818fadb06d5a5cc4c61
MateuszB <mateusz.bankowski6693@gmail.com>
2022-05-10 17:54:54 +0200

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports:[
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
