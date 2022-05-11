df196f67702880719ddfbc4ee0db89f4b3d1df5b
MateuszB <mateusz.bankowski6693@gmail.com>
2022-05-12 00:13:50 +0200

    Add Sidebar component

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+//import { TokenService } from 'src/app/service/token.service';
+import { routes } from '../../consts/routes';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+  public routes: typeof routes = routes;
+
+  showMenuContract = false;
+  showMenuVendor = false;
+  showMenuContact = false;
+  showSettings = false;
+  showMenuRequest = false;
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+
+  shouldRun2 = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+
+  toggleMenuContract() {
+    this.showMenuContract = !this.showMenuContract;
   }
 
+  toggleMenuVendor() {
+    this.showMenuVendor = !this.showMenuVendor;
+  }
+
+  toggleMenuContact() {
+    this.showMenuContact = !this.showMenuContact;
+  }
+
+  toggleMenuRequest() {
+    this.showMenuRequest = !this.showMenuRequest;
+  }
+
+  toggleSettings() {
+    this.showSettings = !this.showSettings;
+  }
+
+  logOut(): void {
+    //this.tokenService.logOut();
+  }
 }
