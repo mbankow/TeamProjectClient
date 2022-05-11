912094ceb6d08d526d9f5b458bbbefe3f182282c
MateuszB <mateusz.bankowski6693@gmail.com>
2022-05-12 00:13:19 +0200

    Add Header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { routes } from 'src/app/consts';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() isMenuOpened!: boolean;
+  @Output() isShowSidebar = new EventEmitter<boolean>();
+  public routers: typeof routes = routes;
+  public openMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
+    this.isShowSidebar.emit(this.isMenuOpened);
   }
-
 }
