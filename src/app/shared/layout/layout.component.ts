355c8b2a2368f839bc1d6dfe4aa45c408f1c2ce2
MateuszB <mateusz.bankowski6693@gmail.com>
2022-05-12 00:14:23 +0200

    Add Layout component

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LayoutComponent implements OnDestroy {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  public isShowSidebar: boolean;
+  public mobileQuery: MediaQueryList;
+  private mobileQueryListener: () => void;
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 1024px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+    this.isShowSidebar = !this.mobileQuery.matches;
   }
-
+  public ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.mobileQueryListener);
+    this.sidenav.close();
+  }
 }
